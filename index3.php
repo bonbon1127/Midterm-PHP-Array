f69d5cb3bf8a9e2ce3c615fc91b100f58110fa74
bonbon1127 <bonrobin1127@gmail.com>
2019-02-04 15:09:25 +0800

    abcd

---
+++ index3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$person = array("Peter = 35", "Ben = 37", "Joe = 43");
+$arrlength = count($cars);
+
+for($x = 0; $x < $arrlength; $x++) {
+    echo $person[$x];
+    echo "<br>";
+}
+?>
+
+</body>
+</html>
